246764b67fc1c782cd70e72c9d748f06fed8df6d
RAHAMSHAIK <81603735+RAHAMSHAIK007@users.noreply.github.com>
2025-07-28 18:19:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-0f918f7e67a3323f0"
 instance_type = "t2.medium"
 key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+vpc_security_group_ids = ["sg-0da91ed9997d755fc"]
 tags = {
 Name = var.instance_names[count.index]
 }
